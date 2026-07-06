cddd036bf96db9a39c93e224205920e9c0ce2822
GHUFRAN-SIDDIQUI <ghufransiddiqui@zhcet.ac.in>
2026-07-06 14:18:58 +0530

    Files added

---
+++ template.sh
@@ -0,0 +1,14 @@
+#Creating the directory
+mkdir -p src
+mkdir -p research
+
+#Creating Files
+touch src/_init_.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Project structure created successfully"
